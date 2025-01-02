0d9b855e8eadbae0a9063a726efd3c9470025782
JoaoMichelS <jovitor931@gmail.com>
2025-01-02 16:50:05 -0300

    Email de recuperação de senha

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ForgotPasswordController;
 
 // Rotas públicas
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/password/forgot', [ForgotPasswordController::class, 'sendResetCode']);
+Route::post('/password/verify', [ForgotPasswordController::class, 'verifyResetCode']);
+Route::post('/password/reset', [ForgotPasswordController::class, 'resetPassword']);
 
 // Rotas protegidas por Sanctum
 Route::middleware('auth:sanctum')->group(function () {
